97470a858f266d3cc602ed92e727787979b3b32f
Marcos Vitor <marcosvitor1994@gmail.com>
2025-07-01 19:00:16 -0300

    Fix Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { GoogleOAuthProvider } from "@react-oauth/google"
-import { AuthProvider } from "./contexts/AuthContext"
-import ProtectedRoute from "./components/ProtectedRoute"
-import Dashboard from "../components/Dashboard"
 import "./App.css"
+import { AuthProvider } from "./contexts/AuthContext"
+import ProtectedRoute from "./components/ProtectedRout"
+import Dashboard from "./components/dashboard"
 
 // Substitua pela sua Google Client ID
 const GOOGLE_CLIENT_ID = "671833846107-d3b90bnlm7bn7aril9klj0oaooe0m0cs.apps.googleusercontent.com"
